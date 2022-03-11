2007a28014e6fcd41ec92ac0792734c307daa4ae
Zhu Panda <zhupandahugo@gmail.com>
2022-03-11 22:09:58 +0800

    refactor code

--- ZP.PowerShell.WinFTAManager.psm1
+++ ZP.PowerShell.WinFTAManager.psm1
@@ -1,3 +1,51 @@
+Function ZP-GetWinTypeAssoc
+{
+    [CmdletBinding(PositionalBinding = $False)]
+    Param
+    (
+        [Parameter(ParameterSetName = "Extension")]
+        [Switch]
+        $Extension,
+        [Parameter(ParameterSetName = "Protocol")]
+        [Switch]
+        $Protocol,
+        [Parameter(Position = 0, Mandatory)]
+        [Parameter(ParameterSetName = "Extension")]
+        [Parameter(ParameterSetName = "Protocol")]
+        [AllowEmptyString()]
+        [String]
+        $Type
+    )
+    If ((-Not $Extension) -And (-Not $Protocol))
+    {
+        Write-Error "?????"
+    }
+    $GetAllMsg = "Get $($Extension ? "File" : "Protocol") Type Association List"
+    $GetTypeMsg = "Get $($Extension ? "File" : "Protocol") Type Association for `"$($Extension ? "." : [String]::Empty)$($Type)`""
+    $AssocListPath = "HKCU:\Software\Microsoft\Windows\$($Extension ? "CurrentVersion\Explorer\FileExts" : "Shell\Associations\UrlAssociations")\*"
+    $AssocTypePath = "HKCU:\Software\Microsoft\Windows\$($Extension ? "CurrentVersion\Explorer\FileExts\.$($Type)" : "Shell\Associations\UrlAssociations\$($Type)")\UserChoice"
+    Write-Verbose ($Type ? $GetTypeMsg : $GetAllMsg)
+    Write-Verbose "Getting from $($Type ? $AssocTypePath : $AssocListPath)"
+    If ($Type)
+    {
+        Write-Output @{$Type = (Get-ItemProperty $AssocTypePath -ErrorAction SilentlyContinue).ProgId}
+    }
+    Else
+    {
+        If ($Extension)
+        {
+            Write-Output (Get-ChildItem $AssocListPath | Where-Object -FilterScript {$_.PSChildName.StartsWith(".")} | ForEach-Object {
+                @{$_.PSChildName.Substring(1) = (Get-ItemProperty "$($_.PSParentPath)\$($_.PSChildName)\UserChoice" -ErrorAction SilentlyContinue).ProgId}
+            })
+        }
+        Else
+        {
+            Write-Output (Get-ChildItem $AssocListPath | ForEach-Object {
+                @{$_.PSChildName = (Get-ItemProperty "$($_.PSParentPath)\$($_.PSChildName)\UserChoice" -ErrorAction SilentlyContinue).ProgId}
+            })
+        }
+    }
+}
 Function ZP-GetFTA
 {
     [CmdletBinding(PositionalBinding = $False)]
@@ -9,20 +57,13 @@ Function ZP-GetFTA
     )
     If ($Extension)
     {
-        Write-Verbose "Get File Type Association for `".$($Extension)`""
-        $AssocFile = (Get-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.$($Extension)\UserChoice" -ErrorAction SilentlyContinue).ProgId
-        Write-Output $AssocFile
+        Write-Output ".$($Extension) => $((ZP-GetWinTypeAssoc -Type $Extension -Extension).Values[0])"
     }
     Else
     {
-        Write-Verbose "Get File Type Association List"
-        $AssocList = Get-ChildItem "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\*" |
-        Where-Object -FilterScript {$_.PSChildName.StartsWith(".")} |
-        ForEach-Object {
-            $ProgId = (Get-ItemProperty "$($_.PSParentPath)\$($_.PSChildName)\UserChoice" -ErrorAction SilentlyContinue).ProgId
-            "$($_.PSChildName) => $ProgId"
-        }
-        Write-Output $AssocList
+        Write-Output (ZP-GetWinTypeAssoc -Type "" -Extension | ForEach-Object {
+            ".$($_.Keys[0]) => $($_.Values[0])"
+        })
     }
 }
 Function ZP-GetPTA
@@ -36,18 +77,12 @@ Function ZP-GetPTA
     )
     If ($Protocol)
     {
-        Write-Verbose "Get Protocol Type Association for `"$($Protocol)`""
-        $AssocFile = (Get-ItemProperty "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\$Protocol\UserChoice" -ErrorAction SilentlyContinue).ProgId
-        Write-Output $AssocFile
+        Write-Output "$($Protocol) => $((ZP-GetWinTypeAssoc -Type $Protocol -Protocol).Values[0])"
     }
     Else
     {
-        Write-Verbose "Get Protocol Type Association List"
-        $AssocList = Get-ChildItem "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\*" |
-        ForEach-Object {
-            $ProgId = (Get-ItemProperty "$($_.PSParentPath)\$($_.PSChildName)\UserChoice" -ErrorAction SilentlyContinue).ProgId
-            "$($_.PSChildName) => $ProgId"
-        }
-        Write-Output $AssocList
+        Write-Output (ZP-GetWinTypeAssoc -Type "" -Protocol | ForEach-Object {
+            "$($_.Keys[0]) => $($_.Values[0])"
+        })
     }
 }
